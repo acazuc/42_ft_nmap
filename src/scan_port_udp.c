f0cd06d16047f6e683c12995d1e9c5bde5534acd
Jukino <acazuc@student.42.fr>
2016-10-09 21:41:30 +0200

    Better udp scan

--- src/scan_port_udp.c
+++ src/scan_port_udp.c
@@ -3,44 +3,41 @@
 void scan_port_udp(t_thread_arg *thread_arg, struct iphdr *ip_header, int port)
 {
 	t_udp_packet packet;
-	size_t started;
 	int received;
 	struct pollfd fds;
-	int tries;
+	int retry;
 
-	tries = 0;
-	while (tries < 3 && !received)
+	retry = 0;
+	received = 0;
+	packet.ip_header = *ip_header;
+	forge_udphdr(&packet, port);
+	packet_flush_icmp(thread_arg->host, port);
+	while (retry < 3 && !received)
 	{
-		packet.ip_header = *ip_header;
-		forge_udphdr(&packet, port);
-		packet_flush_icmp(thread_arg->host, port);
 		if (sendto(thread_arg->host->socket_udp, &packet, sizeof(packet), 0, thread_arg->host->addr, thread_arg->host->addrlen) == -1)
 		{
 			ft_putendl_fd("ft_nmap: failed to send packet", 2);
 			exit(EXIT_FAILURE);
 		}
-		started = epoch_micro();
-		received = 0;
-		while (1)
+		int looper = 0;
+		while (looper < 10)
 		{
-			int timer = 1000000 - (epoch_micro() - started);
-			if (timer < 0)
-				break;
-			fds.fd = 0;
-			fds.events = POLLIN;
-			fds.revents = 0;
-			if (poll(&fds, 0, timer / 1000) == -1)
-			{
-				ft_putendl_fd("ft_nmap: poll failed", 2);
-				exit(EXIT_FAILURE);
-			}
 			if (packet_get_icmp(thread_arg->host, port))
 			{
 				received = 1;
 				break;
 			}
+			fds.fd = 0;
+			fds.events = POLLIN;
+			fds.revents = 0;
+			if (poll(&fds, 0, 100) == -1)
+			{
+				ft_putendl_fd("ft_nmap: poll failed", 2);
+				exit(EXIT_FAILURE);
+			}
+			looper++;
 		}
-		tries++;
+		retry++;
 	}
 	packet_flush_icmp(thread_arg->host, port);
 	if (received)
